379a679d5a9a592d7bdaadef08888c5db8b1bda0
Matosoe <eduardo.matoso@gmail.com>
2019-11-02 23:43:36 -0300

    colocado codigo para produtor e consumidor, produtor integrado, ainda nao testado

--- src/main/java/br/com/perfilcar/perfil/resources/PerfilResource.java
+++ src/main/java/br/com/perfilcar/perfil/resources/PerfilResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.perfilcar.perfil.kafka.Producer;
 import br.com.perfilcar.perfil.models.Perfil;
 import br.com.perfilcar.perfil.repository.PerfilRepository;
 
@@ -19,46 +20,41 @@ import br.com.perfilcar.perfil.repository.PerfilRepository;
 @RequestMapping(path = "/perfil")
 public class PerfilResource {
 
+	
+	private final Producer producer = null;
+	
 	@Autowired
 	PerfilRepository perfilRepository;
 	
 	
 	@GetMapping("/perfis")
 	public List<Perfil> listaPerfis(){
+		producer.send("Pedido de Lista de Perfil");
 		return perfilRepository.findAll();
 	}
 	
 	@GetMapping("/perfil/{id}")
 	public Perfil listaPerfilUnico(@PathVariable(value = "id") long id){
+		producer.send("Pedido de Lista de Perfil por id");
 		return perfilRepository.findById(id);
 	}
 	
 	@PostMapping("/perfil")
 	public Perfil salvaPerfil(@RequestBody Perfil perfil) {
+		producer.send("Pedido de criação de Perfil");
 		return perfilRepository.save(perfil);
 	}
 
 	@PutMapping("/perfil")
 	public Perfil atualizaPerfil(@RequestBody Perfil perfil) {
+		producer.send("Pedido de atualização de Perfil");
 		return perfilRepository.save(perfil);
 	}
 
 	@DeleteMapping("/perfil")
 	 public void deletaPerfil(@RequestBody Perfil perfil) {
+		producer.send("Pedido de deleção de Perfil");
 		perfilRepository.delete(perfil);
 	}
 
-	
-	/*@DeleteMapping("/perfil")
-	public Perfil deletaPerfil=Perfil(@RequestBody Perfil perfil) {
-		perfilRepository.delete(perfil);
-	}
-		*/
-/*	@DeleteMapping("/Perfil")
-	public void Perfil deletaPerfil(@RequestBody Perfil perfil) {
-		perfilRepository.delete(perfil);
-	}
-	
-	
-	*/
 }
